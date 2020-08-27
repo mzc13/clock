1198edfbb904a00a01acd1b7f25766f0cd07f4c3
Muhmmad Choudhary <cho.muhmmad@gmail.com>
2020-08-27 09:54:35 -0400

    Verison 1.2: Added audio and flashing on adhan

--- app.js
+++ app.js
@@ -4,13 +4,45 @@ let tableBody = document.getElementById("tableBody");
 let gDate = document.getElementById("gDate");
 let hDate = document.getElementById("hDate");
 let adhanElements = {
-    fajr: document.getElementById('fajrAdhan'),
-    sunrise: document.getElementById('sunrise'),
-    dhuhr: document.getElementById('dhuhrAdhan'),
-    asr: document.getElementById('asrAdhan'),
-    maghrib: document.getElementById('maghribAdhan'),
-    maghribIqamah: document.getElementById('maghribIqamah'),
-    isha: document.getElementById('ishaAdhan')
+    'fajr': document.getElementById('fajrAdhan'),
+    'sunrise': document.getElementById('sunrise'),
+    'dhuhr': document.getElementById('dhuhrAdhan'),
+    'asr': document.getElementById('asrAdhan'),
+    'maghrib': document.getElementById('maghribAdhan'),
+    'isha': document.getElementById('ishaAdhan'),
+    'jummah': document.getElementById('jummahTimes')
+};
+let iqamahElements = {
+    fajr: document.getElementById('fajrIqamah'),
+    dhuhr: document.getElementById('dhuhrIqamah'),
+    asr: document.getElementById('asrIqamah'),
+    maghrib: document.getElementById('maghribIqamah'),
+    isha: document.getElementById('ishaIqamah'),
+};
+let now = new Date();
+let adhanTimes = {
+    fajr: now,
+    sunrise: now,
+    dhuhr: now,
+    asr: now,
+    maghrib: now,
+    isha: now,
+    jummah: now
+};
+let iqamahTimes = {
+    fajr: todayTime("5:20 AM"),
+    dhuhr: todayTime("1:30 PM"),
+    asr: todayTime("5:00 PM"),
+    maghrib: now,
+    isha: todayTime("9:15 PM")
+};
+let switchHighlightTo = {
+    fajr: now,
+    sunrise: now,
+    dhuhr: now,
+    asr: now,
+    maghrib: now,
+    isha: now
 };
 let tableRows = {
     'fajr': document.getElementById('fajrRow'),
@@ -21,7 +53,26 @@ let tableRows = {
     'isha': document.getElementById('ishaRow')
 };
 let adhanUpdateTimeout = -1;
-let fajrDate, sunriseDate, dhuhrDate, asrDate, maghribDate, ishaDate;
+// let fajrDate : Date,
+//     sunriseDate : Date,
+//     dhuhrDate : Date,
+//     asrDate : Date,
+//     maghribDate : Date,
+//     ishaDate : Date;
+/**
+ * Returns a Date object for today's date at the specified time.
+ * @param time Time string in 24 hour format or 12 hour format
+ */
+function todayTime(time) {
+    return new Date(new Date().toLocaleDateString() + " " + time);
+}
+/**
+ * Returns the input date object rounded down to the nearest minute.
+ * @param date A Date object
+ */
+function truncateSeconds(date) {
+    return todayTime(date.getHours() + ':' + date.getMinutes());
+}
 function setDate() {
     let now = new Date();
     let updateInterval = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59, 0).valueOf() - now.valueOf();
@@ -29,6 +80,7 @@ function setDate() {
     adhanUpdateTimeout = setTimeout(reloadPage, updateInterval + 300000);
     let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     gDate.innerText = now.toLocaleDateString(undefined, options);
+    // Modify this so it fetches from the iqamah server to save time
     fetch('https://api.aladhan.com/v1/timings?latitude=40.68&longitude=-74.11')
         .then(response => (response.ok) ? response.json() : Promise.reject())
         .then(json => setHijriDate(json.data))
@@ -41,6 +93,7 @@ function setDate() {
     });
 }
 function reloadPage() {
+    // TODO change this when deploying new app probably
     fetch('http://bayonnemuslimsclock.com.s3-website-us-east-1.amazonaws.com/')
         .then(res => (res.ok) ? location.reload() : Promise.reject())
         .catch(() => {
@@ -79,38 +132,81 @@ function setHijriDate(data) {
 function hightlight(tr) {
     tr.className = 'highlight';
 }
-function checkIfAdhan() {
-    let now = new Date();
+function unhighlight() {
     for (let tr of Object.values(tableRows)) {
         tr.className = '';
     }
-    if (now < fajrDate) {
-        body.className = 'isha';
-        hightlight(tableRows['isha']);
-    }
-    else if (now < sunriseDate) {
+}
+function checkIfHighlightSwitch(now) {
+    unhighlight();
+    //
+    if (now < switchHighlightTo['sunrise']) {
         body.className = 'fajr';
         hightlight(tableRows['fajr']);
     }
-    else if (now < dhuhrDate) {
+    else if (now < switchHighlightTo['dhuhr']) {
         body.className = 'sunrise';
         hightlight(tableRows['sunrise']);
     }
-    else if (now < asrDate) {
+    else if (now < switchHighlightTo['asr']) {
         body.className = 'dhuhr';
         hightlight(tableRows['dhuhr']);
     }
-    else if (now < maghribDate) {
+    else if (now < switchHighlightTo['maghrib']) {
         body.className = 'asr';
         hightlight(tableRows['asr']);
     }
-    else if (now < ishaDate) {
+    else if (now < switchHighlightTo['isha']) {
         body.className = 'maghrib';
         hightlight(tableRows['maghrib']);
     }
-    else {
+    else if (now < switchHighlightTo['fajr']) {
         body.className = 'isha';
         hightlight(tableRows['isha']);
+    }
+    else {
+        body.className = 'fajr';
+        hightlight(tableRows['fajr']);
+    }
+    checkIfAdhan(now);
+}
+function checkIfAdhan(now) {
+    for (let adhan of Object.entries(adhanTimes)) {
+        // TODO This might need to be modified when implementing jummah but possibly not
+        if (adhan[1].valueOf() == now.valueOf()) {
+            // TODO need to ensure that adhanTimes and adhanElements keep the same key values
+            playAdhan(adhan[0]);
+        }
+    }
+}
+function checkIfIqamah(now) {
+    const oneMin = 60000;
+    for (let time of Object.values(iqamahTimes)) {
+        if ((now.valueOf() + oneMin) == time.valueOf()) {
+            setTimeout(playIqamah, 40 * 1000);
+        }
+    }
+}
+function minuteChecks() {
+    let now = truncateSeconds(new Date());
+    checkIfHighlightSwitch(now);
+    checkIfAdhan(now);
+    checkIfIqamah(now);
+}
+function playAdhan(adhanName) {
+    let audio = new Audio('takbeer.mp3');
+    audio.play();
+    flashPrayer(adhanName, 20, 700);
+}
+function playIqamah() {
+    let audio = new Audio('lasalah.mp3');
+    audio.play();
+}
+function flashPrayer(adhanName, times, delay) {
+    setTimeout(unhighlight, delay);
+    setTimeout(hightlight.bind(null, tableRows[adhanName]), delay * 1.9);
+    if (times > 1) {
+        setTimeout(flashPrayer.bind(null, adhanName, times - 1, delay), delay * 2);
     }
 }
 function islamicFinderAdhan(res) {
@@ -130,24 +226,27 @@ function islamicFinderAdhan(res) {
         adhanElements['maghrib'].innerText = removeStart0(maghrib);
         adhanElements['isha'].innerText = removeStart0(isha);
         let today = new Date().toLocaleDateString();
-        fajrDate = new Date(today + " " + fajr);
-        sunriseDate = new Date(today + " " + sunrise);
-        dhuhrDate = new Date(today + " " + dhuhr);
-        asrDate = new Date(today + " " + asr);
-        maghribDate = new Date(today + " " + maghrib);
-        ishaDate = new Date(today + " " + isha);
-        let maghribIqamah = new Date(maghribDate.getTime() + 5 * 60000);
+        adhanTimes['fajr'] = new Date(today + " " + fajr);
+        adhanTimes['sunrise'] = new Date(today + " " + sunrise);
+        adhanTimes['dhuhr'] = new Date(today + " " + dhuhr);
+        adhanTimes['asr'] = new Date(today + " " + asr);
+        adhanTimes['maghrib'] = new Date(today + " " + maghrib);
+        adhanTimes['isha'] = new Date(today + " " + isha);
+        iqamahTimes['maghrib'] = new Date(adhanTimes['maghrib'].getTime() + 5 * 60000);
+        let mIqamah = iqamahTimes['maghrib'];
         // TODO fix this so the function is more robust and these extra sanitation steps dont have to happen here
-        let mHour = (maghribIqamah.getHours() < 10) ? "0" + maghribIqamah.getHours() : "" + maghribIqamah.getHours();
-        let mMin = (maghribIqamah.getMinutes() < 10) ? "0" + maghribIqamah.getMinutes() : "" + maghribIqamah.getMinutes();
-        adhanElements['maghribIqamah'].innerText = formatTime(mHour + ':' + mMin);
-        // console.log(fajrDate);
-        // console.log(sunriseDate);
-        // console.log(dhuhrDate);
-        // console.log(asrDate);
-        // console.log(maghribDate);
-        // console.log(ishaDate);
-        checkIfAdhan();
+        let mHour = (mIqamah.getHours() < 10) ? "0" + mIqamah.getHours() : "" + mIqamah.getHours();
+        let mMin = (mIqamah.getMinutes() < 10) ? "0" + mIqamah.getMinutes() : "" + mIqamah.getMinutes();
+        iqamahElements['maghrib'].innerText = formatTime(mHour + ':' + mMin);
+        // TODO fix this so if you end up adding to a time after midnight it still works
+        switchHighlightTo['fajr'] = new Date(iqamahTimes['isha'].valueOf() + 20 * 60000);
+        switchHighlightTo['sunrise'] = new Date(iqamahTimes['fajr'].valueOf() + 20 * 60000);
+        // Deal with special case of jummah
+        switchHighlightTo['dhuhr'] = new Date(adhanTimes['sunrise'].valueOf() + 20 * 60000);
+        switchHighlightTo['asr'] = new Date(iqamahTimes['dhuhr'].valueOf() + 20 * 60000);
+        switchHighlightTo['maghrib'] = new Date(iqamahTimes['asr'].valueOf() + 20 * 60000);
+        switchHighlightTo['isha'] = new Date(iqamahTimes['maghrib'].valueOf() + 20 * 60000);
+        checkIfHighlightSwitch(new Date());
     }
     catch (error) {
         errorMode('Failed setting Adhan times');
@@ -170,4 +269,4 @@ function errorFixed() {
 }
 setDate();
 clockApp();
-window.addEventListener('minutePassed', checkIfAdhan);
+window.addEventListener('minutePassed', minuteChecks);
